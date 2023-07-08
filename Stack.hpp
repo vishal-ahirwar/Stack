0927134f48b7a19a92547c3c2318a7aa54c17669
vishal-ahirwar <vishalchoudhry0091@gmail.com>
2023-07-08 23:53:51 +0530

    Add project files.

---
+++ Stack.hpp
@@ -0,0 +1,23 @@
+#ifndef STACK
+#define STACK
+#define STACK_MACRO template<typename DATA>
+
+STACK_MACRO
+class Stack
+{
+private:
+	int size{};
+	int top{ -1 };
+	DATA* data{nullptr};
+public:
+	//TODO Push()
+	//TODO Pop()
+	//TODO Peak()
+	//TODO isEmpty()
+	//TODO isFull()
+	//TODO Top()
+	//TODO LinkedList Implementation
+	//TODO Parenthesis Matching
+
+};
+#endif//STACk
